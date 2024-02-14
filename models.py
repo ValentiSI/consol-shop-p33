a60c7c98c2e7d5603c75fe6dd17165d8a8d3e2d2
ValentiSI <tinkamv@gmail.com>
2024-02-14 21:31:57 +0300

    second

---
+++ models.py
@@ -0,0 +1,11 @@
+from simple_sqlite_orm import Table, column_types
+
+class User(Table):
+    id = column_types.IdColumn()
+    email = column_types.TextColumn(unique=True)
+    password = column_types.TextColumn()
+    
+class Product(Table):
+    id = column_types.IdColumn()
+    title = column_types.TextColumn(unique=True)
+    price = column_types.NumericColumn()
