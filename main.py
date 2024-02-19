2dd4a511a4337984e2228fe290f726da653f645f
Alex <75857847+deustenebrarum@users.noreply.github.com>
2024-02-19 20:36:22 +0300

    4 create project main function (#5)
    * docs: Add README for project building
    
    * refactor: Fix code style errors across whole project
    
    * feat: Add project main file

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print('App started')
+
+
+if __name__ == '__main__':
+    main()
--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 from simple_sqlite_orm import Table, column_types
 
+
 class User(Table):
     id = column_types.IdColumn()
     email = column_types.TextColumn(unique=True)
     password = column_types.TextColumn()
-    
+
+
 class Product(Table):
     id = column_types.IdColumn()
     title = column_types.TextColumn(unique=True)
