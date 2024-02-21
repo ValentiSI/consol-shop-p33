5f6519cf0b3c30520263ee0676ae72abe1843957
ValentiSI <tinkamv@gmail.com>
2024-02-21 21:39:18 +0300

    feat: Add product into basket feature

--- products.py
+++ products.py
@@ -21,15 +21,6 @@ def display_products(db_context: DB):
         )
 
 
-def select_product(db_context: DB, prod_id: int):
-    prod_id = int(input("Enter product id: "))
-    product = db_context.products.get(prod_id)
-    if not product:
-        print("No such product")
-        return
-    print(f"{product.title} | {product.price:.2f}$")
-
-
 # pandas variant
 # def display_products(db_context: DB):
 #     products = list(db_context.products.getAll())
@@ -41,22 +32,30 @@ def select_product(db_context: DB, prod_id: int):
 #     print(df.to_string(index=False))
 
 
-def add_product_in_basket(db, user_id, product_id):
+def add_product_in_basket(db_context: DB, user_id):
+    product_id = int(input("Enter product id: "))
+    
+    product = db_context.products.get(product_id)
+    if not product:
+        print("No such product")
+        return
+    print(f"{product.title} | {product.price:.2f}$")
+    
     products = list(
-        db.products_in_basket.getAll(
+        db_context.products_in_basket.getAll(
             filter=f"product_id = {product_id} AND user_id = {user_id}"
         )
     )
 
     if len(products) == 0:
-        db.products_in_basket.insert(
-            db.products_in_basket.model(
+        db_context.products_in_basket.insert(
+            db_context.products_in_basket.model(
                 None, product_id, user_id, input("Введите количество: ")
             )
         )
     else:
         existing_product = products[0]
         existing_product.count += int(input("Введите количество: "))
-        db.products_in_basket.update(existing_product)
+        db_context.products_in_basket.update(existing_product)
 
     print("Товар добавлен в корзину")
